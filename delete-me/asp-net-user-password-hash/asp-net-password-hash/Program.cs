971dc4f8059d1b8dcadfd9ae97a49282cd728f1e
philandersonsparta <panderson@spartaglobal.com>
2019-02-08 10:43:03 +0000

    ASP.NET Hash

---
+++ delete-me/asp-net-user-password-hash/asp-net-password-hash/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace asp_net_password_hash
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var base64string = "AQAAAAEAACcQAAAAELZyWXHYmPNCQb8Ah5iXYHEsxh84uzoQS8/Nb1lkQ7bXPCJtdG3Wk4g/B9dgI0KzNQ==";
+
+            var decodedbase64 = Convert.FromBase64String(base64string);
+
+            
+
+            foreach(var b in decodedbase64)
+            {
+                Console.WriteLine(b);
+                File.AppendAllText("output.txt", b.ToString()+"\r\n");
+                
+            }
+
+            System.Diagnostics.Process.Start("notepad.exe", "output.txt");
+        }
+    }
+}
